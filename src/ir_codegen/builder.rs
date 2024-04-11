a034268f9768cbf5bb78775d1d92e6fe328c4625
Caleb L'Italien <caleblitalien@proton.me>
2024-04-11 12:20:26 -0400

    WIP

--- src/ir_codegen/builder.rs
+++ src/ir_codegen/builder.rs
@@ -1,7 +1,6 @@
 extern crate llvm_sys as llvm;
 
 use llvm::{core, prelude::*}; // TODO change to not use wild star import
-use std::ffi::CString;
 
 /// Creates a builder in context
 pub fn create_builder(context: LLVMContextRef) -> LLVMBuilderRef {
--- tests/ir_codegen/block_tests.rs
+++ tests/ir_codegen/block_tests.rs
@@ -1,22 +1,36 @@
+extern crate llvm_sys as llvm;
 
-use super::*;
+use llvm::prelude::{LLVMBasicBlockRef, LLVMBuilderRef, LLVMContextRef, LLVMValueRef};
 use mockall::mock;
+
+use safe_llvm::{api::api_definition::LLVMApi, memory_management::ir_pointer::IRPointer};
 
 mock! {
     LLVMApi {}
     impl LLVMApi for LLVMApi {
-        fn append_basic_block_in_context(&self, context: LLVMContextRef, function: LLVMValueRef, name: &str) -> LLVMBasicBlockRef;
+        fn create_basic_block(&self, context: LLVMContextRef, function: LLVMValueRef, name: &str) -> IRPointer<LLVMBasicBlockRef>;
+            fn get_current_block(&self, builder: *mut llvm::LLVMBuilder) -> IRPointer<LLVMBasicBlockRef>;
+            fn create_cond_br(&self, builder: LLVMBuilderRef, condition: LLVMValueRef, then_bb: LLVMBasicBlockRef, else_bb: LLVMBasicBlockRef) -> IRPointer<LLVMValueRef>;
+            fn create_br(&self, builder: LLVMBuilderRef, target_bb: LLVMBasicBlockRef) -> IRPointer<LLVMValueRef>;
+            fn insert_before_basic_block(&self, context: LLVMContextRef, before_target: LLVMBasicBlockRef, name: &str) -> IRPointer<LLVMBasicBlockRef>;
+            fn position_builder(&self, builder: *mut llvm::LLVMBuilder, bb: *mut llvm::LLVMBasicBlock);
+            fn delete_basic_block(&self, bb: LLVMBasicBlockRef);
+            fn get_first_instruction(&self, bb: LLVMBasicBlockRef) -> IRPointer<LLVMValueRef>;
+            fn get_last_instruction(&self, bb: LLVMBasicBlockRef) -> IRPointer<LLVMValueRef>;   
+            fn create_phi(&self, builder: LLVMBuilderRef, possible_values: &[(LLVMValueRef, LLVMBasicBlockRef)], name: &str) -> IRPointer<LLVMValueRef>; 
     }
 }
 
 #[test]
 fn test_create_basic_block() {
     let mut mock_api = MockLLVMApi::new();
-    mock_api.expect_append_basic_block_in_context()
-            .returning(|_, _, _| std::ptr::null_mut()); 
+    let raw_ptr = IRPointer::new(Some(std::ptr::null_mut()));
+
+    mock_api.expect_create_basic_block()
+            .returning(|_, _, _| raw_ptr); 
 
     let context = std::ptr::null_mut();
     let function = std::ptr::null_mut();
-    let bb = create_basic_block(&mock_api, context, function, "test");
+    let bb = (&mock_api).create_basic_block(context, function, "test");
     assert!(bb.is_null(), "Expected null pointer return for basic block");
 }
