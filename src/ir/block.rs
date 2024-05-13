1c79da015b75708af385da93e2d0761fda5225e0
John Daly <dalyj@union.edu>
2024-05-13 14:53:15 -0400

    added experimental get next block

--- src/ir/block.rs
+++ src/ir/block.rs
@@ -120,6 +120,37 @@ impl ResourcePools {
             self.get_basic_block_tag(block)
         }
     }
+
+    /// Retrieves the next basic block after the current one. 
+    pub fn get_next_block(&mut self, builder_tag: BuilderTag) -> Option<BasicBlockTag> {
+        let builder_arc_rwlock = self.get_builder(builder_tag)?;
+
+        let builder_ptr: LLVMBuilderRef = {
+            let builder_rwlock = builder_arc_rwlock.read().expect("Failed to lock builder for reading");
+            let builder_ref = builder_rwlock.read(LLVMRefType::Builder, |builder_ref| {
+                match builder_ref {
+                    LLVMRef::Builder(ptr) => Some(*ptr),
+                    _ => None
+                }
+            })?;
+            builder_ref
+        };
+
+        let block = unsafe {
+            core::LLVMGetInsertBlock(builder_ptr)
+        };
+
+        if block.is_null() {
+            None
+        } else {
+            let return_block = unsafe {
+                core::LLVMGetNextBasicBlock(block)
+            };
+
+            self.get_basic_block_tag(return_block)
+        }
+
+    }
     
     /// Creates a conditional branch to two different blocks. 
     pub fn create_cond_br(&mut self, builder_tag: BuilderTag, condition_tag: ValueTag, then_bb_tag: BasicBlockTag, else_bb_tag: BasicBlockTag) -> Option<ValueTag> {
